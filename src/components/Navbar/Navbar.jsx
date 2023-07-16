cce8f245d3780d4845aca29711a6418ac5eecb28
Harbestt <habeebhuszein@gmail.com>
2023-07-16 12:16:51 +0100

    made some changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Andrew</div>
+        <div className="n-name">Habeeb</div>
         <Toggle />
       </div>
       {/* right */}
